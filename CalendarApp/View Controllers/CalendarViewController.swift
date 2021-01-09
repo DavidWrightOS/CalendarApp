d4fe3296d445972056d2d23eab13b341edde45d4
David Wright <davidtwright89@gmail.com>
2021-01-09 03:42:37 -0500

    Added a CalendarViewController

---
+++ CalendarApp/View Controllers/CalendarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewController.swift
+//  CalendarApp
+//
+//  Created by David Wright on 1/9/21.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
