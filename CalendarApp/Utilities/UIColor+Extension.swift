b9ac4b48f9baa4cb098095486cba59bda350a2dc
David Wright <davidtwright89@gmail.com>
2021-01-10 03:23:38 -0500

    Added a custom "barBackgroundColor" to match the default navigation and toolbar background colors

---
+++ CalendarApp/Utilities/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  CalendarApp
+//
+//  Created by David Wright on 1/10/21.
+//
+
+import UIKit
+
+extension UIColor {
+    static var barBackgroundColor: UIColor { UIColor(named: "BarBackgroundColor")! }
+}
