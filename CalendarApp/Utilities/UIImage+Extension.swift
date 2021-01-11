6e872f536cf826e3a9dce3bdb419429e4b58eb15
David Wright <davidtwright89@gmail.com>
2021-01-11 04:35:12 -0500

    Added an extension to UIImage for creating an image with the specified background color

---
+++ CalendarApp/Utilities/UIImage+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIImage+Extension.swift
+//  CalendarApp
+//
+//  Created by David Wright on 1/11/21.
+//
+
+import UIKit
+
+extension UIImage {
+    static func imageWithColor(_ color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
