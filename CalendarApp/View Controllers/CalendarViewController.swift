72ebe2e9e0959922f081dc5335b5b48cf50c0fbe
David Wright <davidtwright89@gmail.com>
2021-01-11 01:57:14 -0500

    Replaced header background view (added to extend nav bar color to the top of the view) with conformance to UIBarPositioningDelegate

--- CalendarApp/View Controllers/CalendarViewController.swift
+++ CalendarApp/View Controllers/CalendarViewController.swift
@@ -11,7 +11,6 @@ class CalendarViewController: UIViewController {
     
     private let toolbar: UIToolbar = {
         let tb = UIToolbar()
-        
         let barButtonItems: [UIBarButtonItem] = [
             UIBarButtonItem(title: "Today", style: .plain, target: self, action: #selector(todayButtonTapped)),
             UIBarButtonItem(systemItem: .flexibleSpace),
@@ -19,51 +18,42 @@ class CalendarViewController: UIViewController {
             UIBarButtonItem(systemItem: .flexibleSpace),
             UIBarButtonItem(title: "Inbox", style: .plain, target: self, action: #selector(inboxButtonTapped))
         ]
-        
         tb.setItems(barButtonItems, animated: false)
         return tb
     }()
             
-    private let navigationBar: UINavigationBar = {
+    lazy private var navigationBar: UINavigationBar = {
         let navBar = UINavigationBar()
         
         let viewStyleButton = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(viewStyleButtonTapped))
         let searchButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchButtonTapped))
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
         
-        let navItem = UINavigationItem()
-        
         let backButton: UIButton = {
             let button = UIButton(type: .system)
-            
             let monthFormatter = DateFormatter()
             monthFormatter.dateFormat = "MMMM"
             let currentMonth = monthFormatter.string(from: Date())
+            
             button.setTitle(" " + currentMonth, for: .normal)
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.setTitleColor(button.tintColor, for: .normal)
             
             let config = UIImage.SymbolConfiguration(textStyle: UIFont.TextStyle.headline, scale: .large)
             let backImage = UIImage(systemName: "chevron.backward")?.withConfiguration(config)
             button.setImage(backImage, for: .normal)
-            
-            button.setTitleColor(button.tintColor, for: .normal)
             button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
             
             return button
         }()
         
+        let navItem = UINavigationItem()
         navItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
         navItem.rightBarButtonItems = [addButton, searchButton, viewStyleButton]
         navBar.setItems([navItem], animated: false)
-        navBar.barTintColor = .barBackgroundColor
-        navBar.isTranslucent = false
+        navBar.delegate = self
         
         return navBar
-    }()
-    
-    private let headerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .barBackgroundColor
-        return view
     }()
     
     override func viewDidLoad() {
@@ -75,13 +65,11 @@ class CalendarViewController: UIViewController {
         view.backgroundColor = .systemBackground
         
         view.addSubview(navigationBar)
+        navigationBar.sizeToFit()
         navigationBar.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor)
         
         view.addSubview(toolbar)
         toolbar.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor)
-        
-        view.insertSubview(headerView, at: 0)
-        headerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: navigationBar.bottomAnchor, right: view.rightAnchor)
     }
     
     // MARK: - Selectors
@@ -112,5 +100,14 @@ class CalendarViewController: UIViewController {
     
     @objc private func addButtonTapped() {
         print("DEBUG: debug addButtonTapped..")
+    }
+}
+
+
+// MARK: - Navigation Bar Delegate
+
+extension CalendarViewController: UINavigationBarDelegate, UIBarPositioningDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
     }
 }
