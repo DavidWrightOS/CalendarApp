7b4776652639286b44ed4b5d0bb198bb1920f701
David Wright <davidtwright89@gmail.com>
2021-01-13 00:35:34 -0500

    Created the table view items

--- CalendarApp/View Controllers/EditEventViewController.swift
+++ CalendarApp/View Controllers/EditEventViewController.swift
@@ -13,6 +13,34 @@ class EditEventViewController: UIViewController {
     
     let tableView = UITableView(frame: .zero, style: .grouped)
     
+    // TableView items by section
+    
+    private var itemsBySection: [[Item]] = {
+        [
+            [   // Section 0
+                Item(type: .textField, placeholder: "Title"),
+                Item(type: .location, placeholder: "Location")
+            ],
+            [   // Section 1
+                Item(type: .allDaySwitch, text: "All-day"),
+                Item(type: .date, text: "Starts", placeholder: "None"),
+                Item(type: .date, text: "Ends", placeholder: "None"),
+                Item(type: .repeats, text: "Repeat", placeholder: "Never"),
+                Item(type: .travelTime, text: "Travel Time", placeholder: "None")
+            ],
+            [   // Section 2
+                Item(type: .alert, text: "Alert", placeholder: "None")
+            ],
+            [   // Section 3
+                Item(type: .attachment, text: "Add attachment...")
+            ],
+            [   // Section 4
+                Item(type: .textField, placeholder: "URL"),
+                Item(type: .textView, placeholder: "Notes")
+            ]
+        ]
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
