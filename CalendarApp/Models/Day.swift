737a72b5bf31a7d3a7a038672228f1e0d9614373
David Wright <davidtwright89@gmail.com>
2021-01-17 23:48:31 -0500

    Added a Day model

---
+++ CalendarApp/Models/Day.swift
@@ -0,0 +1,14 @@
+//
+//  Day.swift
+//  CalendarApp
+//
+//  Created by David Wright on 1/17/21.
+//
+
+import Foundation
+
+struct Day {
+  let date: Date
+  let number: String
+  let isSelected: Bool
+}
