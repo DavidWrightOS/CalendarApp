bc3a916b445753b5224869ed1fd504f1a0928d33
David Wright <davidtwright89@gmail.com>
2021-01-19 17:31:38 -0500

    Update Day model with additional attributes needed to create the MonthCollectionView

--- CalendarApp/Models/Day.swift
+++ CalendarApp/Models/Day.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 struct Day {
-  let date: Date
-  let number: String
-  let isSelected: Bool
+    let date: Date
+    let number: String
+    let isSelected: Bool
+    let isWithinDisplayedMonth: Bool
+    let isWeekend: Bool
+    let hasEvent: Bool
 }
