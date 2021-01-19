fda4d94d54183a8e656c739bc83d9d7fc23c8cad
David Wright <davidtwright89@gmail.com>
2021-01-18 22:54:54 -0500

    Added a MonthViewController

---
+++ CalendarApp/View Controllers/MonthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MonthViewController.swift
+//  CalendarApp
+//
+//  Created by David Wright on 1/18/21.
+//
+
+import UIKit
+
+class MonthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
